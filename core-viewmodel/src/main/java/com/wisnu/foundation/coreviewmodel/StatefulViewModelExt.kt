8f4161a59eece227e189e1ab2e9f898b5e9a7b13
wisnu kurniawan <wisnuko13@gmail.com>
2022-10-05 21:55:35 +0700

    Rename to sample

--- core-viewmodel/src/main/java/com/wisnu/foundation/coreviewmodel/StatefulViewModelExt.kt
+++ core-viewmodel/src/main/java/com/wisnu/foundation/coreviewmodel/StatefulViewModelExt.kt
@@ -1,11 +1,5 @@
 package com.wisnu.foundation.coreviewmodel
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-
 suspend fun <STATE, EFFECT, ACTION, ENVIRONMENT> handleEffect(
     viewModel: StatefulViewModel<STATE, EFFECT, ACTION, ENVIRONMENT>,
     handle: (EFFECT) -> Unit
@@ -19,17 +13,17 @@ suspend fun <STATE, EFFECT, ACTION, ENVIRONMENT> handleEffect(
 }
 
 
-@OptIn(ExperimentalLifecycleComposeApi::class)
-@Composable
-fun <STATE, EFFECT, ACTION, ENVIRONMENT> HandleEffect(
-    viewModel: StatefulViewModel<STATE, EFFECT, ACTION, ENVIRONMENT>,
-    handle: (EFFECT) -> Unit
-) {
-    val effect by viewModel.effect.collectAsStateWithLifecycle()
-    LaunchedEffect(effect) {
-        effect?.let {
-            handle(it)
-            viewModel.resetEffect()
-        }
-    }
-}
+//@OptIn(ExperimentalLifecycleComposeApi::class)
+//@Composable
+//fun <STATE, EFFECT, ACTION, ENVIRONMENT> HandleEffect(
+//    viewModel: StatefulViewModel<STATE, EFFECT, ACTION, ENVIRONMENT>,
+//    handle: (EFFECT) -> Unit
+//) {
+//    val effect by viewModel.effect.collectAsStateWithLifecycle()
+//    LaunchedEffect(effect) {
+//        effect?.let {
+//            handle(it)
+//            viewModel.resetEffect()
+//        }
+//    }
+//}
